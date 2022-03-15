09fd294b474b02434ef6c3b769bfce595a68cf88
Koichi NISHIKAWA <nishikawa@sinq.co.jp>
2022-03-15 21:44:00 +0900

    add routing : hello

--- server/server.go
+++ server/server.go
@@ -9,6 +9,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello my website!")
+	})
 
 	http.ListenAndServe(":80", nil)
 }
