e2f8df97a5fe38a654f5eb6f0b0b602bd7b3c378
Koichi NISHIKAWA <nishikawa@sinq.co.jp>
2022-03-20 15:31:18 +0900

    add: sql-driver

--- src/main.go
+++ src/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/flashcard/article"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
 )
 
+func open(path string, count uint) *sql.DB {
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		log.Fatal("open error:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		time.Sleep(time.Second * 2)
+		count--
+		fmt.Printf("retry... count:%v\n", count)
+		return open(path, count)
+	}
+
+	fmt.Println("db connected!!")
+	return db
+}
+
+func connectDB() *sql.DB {
+	// var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
+	// 	os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
+	// 	os.Getenv("MYSQL_DATABASE"))
+	var path string = fmt.Sprintf("user:password@(127.0.0.1:3306)/article?parsetime=true")
+
+	return open(path, 100)
+}
+
 func main() {
+	db := connectDB()
+	defer db.Close()
+	article.ReadAll(db)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
